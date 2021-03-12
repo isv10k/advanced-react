d25125d8aecd3a62ffbd9995c6db15fdf18f3e92
Sergey Ilchenko <isv10k@gmail.com>
2021-03-12 20:07:13 +0500

    51 search, downshift

--- frontend/lib/paginationField.js
+++ frontend/lib/paginationField.js
@@ -30,9 +30,9 @@ export default function paginationField() {
 
       // If there are items, just return them from the cache, and we don't need to go to the network
       if (items.length) {
-        console.log(
-          `There are ${items.length} items in the cache! Gonna send them to apollo`
-        );
+        // console.log(
+        //   `There are ${items.length} items in the cache! Gonna send them to apollo`
+        // );
         return items;
       }
 
@@ -49,7 +49,7 @@ export default function paginationField() {
     merge(existing, incoming, { args }) {
       const { skip, first } = args;
       // This runs when the Apollo client comes back from the network with our product
-      console.log(`Merging items from the network ${incoming.length}`);
+      // console.log(`Merging items from the network ${incoming.length}`);
       const merged = existing ? existing.slice(0) : [];
       for (let i = skip; i < skip + incoming.length; ++i) {
         merged[i] = incoming[i - skip];
